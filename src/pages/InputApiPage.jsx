ac665316f236f7d925dc81e8097fa181132e7dfe
mutawalle <13521065@std.stei.itb.ac.id>
2023-04-09 19:25:48 +0700

    feat: add routes

---
+++ src/pages/InputApiPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InputApiPage() {
+  return (
+    <div>InputApiPage</div>
+  )
+}
+
+export default InputApiPage
---
+++ src/pages/InputFilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InputFilePage() {
+  return (
+    <div>InputFilePage</div>
+  )
+}
+
+export default InputFilePage
