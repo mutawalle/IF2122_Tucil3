fb43ac45028229d88331cf9b319cd9b9a31a7c8e
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-04-09 23:41:34 +0700

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading,Flex } from '@chakra-ui/react'
 import React from 'react'
 import Footer from '../../components/footer'
 import Navbar from '../../components/navbar'
@@ -8,6 +8,16 @@ function Home() {
     <>
       <Navbar />
       <Box bg="#ffffff" minH="100vh">
+        <Flex flexDir="column" alignItems="center" gap={10} mt={7}>
+          <Heading
+            fontSize={{ base: '4xl', lg: '6xl' }}
+            mb={{ base: 4, md: 10, lg: 16 }}
+            text-align="center"
+          >
+          Shortest Path Finding
+          </Heading>
+          
+        </Flex>
       </Box>
       <Footer/>
     </>
