db63f03f957ee58e85bf17239fc1859cf756c3a2
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-04-10 13:25:56 +0700

    Feat : inputFile

--- src/pages/inputApi/InputApiPage.jsx
+++ src/pages/inputApi/InputApiPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useState } from 'react'
 import Navbar from '../../components/navbar'
-import { Box, Flex, Text, Input, Button } from '@chakra-ui/react'
+import { Box, Flex, Text, Input, Button, } from '@chakra-ui/react'
 import Footer from '../../components/footer'
 
 function InputApiPage() {
